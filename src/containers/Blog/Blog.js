2e608d547d0e8c5091e9054703d7dac402b30dcb
Marissa Douglass <mahdouglass@gmail.com>
2019-09-23 21:24:24 -0400

    If not authenticated new post won't display because of if guard

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,6 +7,10 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
+    state = {
+        auth: false,
+    }
+
     render() {
         return (
             <div className="Blog">
@@ -22,7 +26,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
                     <Route path="/" component={Posts} />
                     <Redirect from="/" to="/posts" />
                 </Switch>
